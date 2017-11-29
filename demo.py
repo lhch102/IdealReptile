31cbbc95d74b6db3e0d7367e6bd8485e44d45d82
lhch <lhch102@yeah.net>
2017-11-29 14:28:16 +0800

    ok

--- demo.py
+++ demo.py
@@ -1,1 +1,14 @@
-print(1111111)
+# -*- coding:utf-8 -*-
+from urllib import request
+
+url = "https://www.baidu.com/"
+# 直接请求
+
+req = request.Request(url)
+req.add_header("User-Agent",
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) "
+               "Chrome/62.0.3202.94 Safari/537.36")
+response = request.urlopen(req)
+
+print(response.read().decode('utf-8'))
+
