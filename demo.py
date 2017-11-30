d5ab351cd31d1cedabb2ca9b6e1e8a2330f5aa7b
lhch <lhch102@yeah.net>
2017-11-30 18:31:56 +0800

    模拟登录成功

--- demo.py
+++ demo.py
@@ -1,17 +1,16 @@
-# -*- coding:utf-8 -*-
-from urllib import request, parse
+import requests
+import json
 
-# 网址
-url = "http://210.51.169.193:8080/login.html"
+url = 'http://210.51.169.193:8080/login.whtml'
 
-headers = {
-    'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                  r'Chrome/43.0.2357.81 Safari/537.36',
-    'Referer': r'http://210.51.169.193:8080/login.html',
-    'Content-Type': r'application/json; charset=UTF-8'
-}
-data = {'user': {'userName': '60000852', 'password': '60000852'}, 'loginURL': 'http://210.51.169.193:8080/login.html'}
-parmas = request.urlencode(data)
-req = request.urlopen(url, parmas)
-
-print(req)
+jsonData = json.dumps(
+    {
+        "user": {
+            "userName": "60000852",
+            "password": "60000852"
+        },
+        "loginURL": "http://210.51.169.193:8080/login.html"
+    })
+response = requests.post(url, data=jsonData, headers={'content-type': 'application/json'})
+s = json.loads(response.text)
+print(s.keys)
